0037a1f9fa5df490ea8e790d6fbb2dd7472554fa
Jo <jo@iMac.kitchenpc.net>
2023-05-27 12:51:55 -0700

    changed quotes.rest api to api.api-ninjas.com/quotes to remove cors restriction

--- src/api/quotes/index.js
+++ src/api/quotes/index.js
@@ -1,9 +1,13 @@
-const API_URL = `https://quotes.rest/qod?language=en`;
+const accessKey = process.env.REACT_APP_API_NINJAS_KEY;
+const API_URL = `https://api.api-ninjas.com/v1/quotes?limit=1`;
 
 export const getQuote = async () => {
-  const response = await fetch(API_URL);
+  const response = await fetch(API_URL, {
+    method: "GET",
+    headers: { "X-Api-Key": accessKey },
+  });
   const json = await response.json();
-  const quote = json.contents.quotes[0];
+  const quote = json[0];
 
   return {
     quote: quote.quote,
